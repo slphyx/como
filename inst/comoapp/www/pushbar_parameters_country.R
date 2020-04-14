aae0752bf8807c6d6a93b286259b97a019afe25d
Sompob Saralamba <sakngoi@hotmail.com>
2020-04-14 11:43:41 +0700

    TH default
    set th as default

--- inst/comoapp/www/pushbar_parameters_country.R
+++ inst/comoapp/www/pushbar_parameters_country.R
@@ -5,14 +5,14 @@ list(
             column(4, includeMarkdown("./www/markdown/about_country_data.md"), includeMarkdown("./www/markdown/about_data.md")),
             column(4,
                    pickerInput("country", label = "Demographic Data:", choices = c("-- Own Value ---", countries_demog),
-                               selected = "Afghanistan",
+                               selected = "Thailand",
                                options = pickerOptions(liveSearch = TRUE)),
                    pickerInput("country_cases", label = "Cases/Deaths:", choices = countries_cases,
-                               selected = "Afghanistan", options = pickerOptions(liveSearch = TRUE))
+                               selected = "Thailand", options = pickerOptions(liveSearch = TRUE))
             ),
             column(4,
                    pickerInput("country_contact", label = "Social Contacts Data:", choices = countries_contact,
-                               options = pickerOptions(liveSearch = TRUE), selected = "Pakistan"),
+                               options = pickerOptions(liveSearch = TRUE), selected = "Thailand"),
                    sliderInput("household_size", label = "Mean household size:", value = 2.5, min = 1, max = 10,
                                step = 0.1, post = " indiv.", ticks = FALSE),
                    numericInput("mean_imports", label = "Mean number of infectious migrants per day:", value = 0, min = 0, max = 500,
